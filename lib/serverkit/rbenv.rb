b47f8bcc69444e01ae15beab3c66794bd5298b61
Ryo Nakamura <r7kamura@gmail.com>
2015-04-07 19:12:04 +0900

    bundle gem serverkit-rbenv

---
+++ lib/serverkit/rbenv.rb
@@ -0,0 +1,7 @@
+require "serverkit/rbenv/version"
+
+module Serverkit
+  module Rbenv
+    # Your code goes here...
+  end
+end
---
+++ lib/serverkit/rbenv/version.rb
@@ -0,0 +1,5 @@
+module Serverkit
+  module Rbenv
+    VERSION = "0.1.0"
+  end
+end
