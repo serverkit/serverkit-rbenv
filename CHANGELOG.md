ea30d375c456a7311f99f844fa1afc7f38915bb8
Ryo Nakamura <r7kamura@gmail.com>
2015-04-11 03:55:23 +0900

    v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.2
+- Add missing runtime dependency on serverkit gem
+
+## 0.0.1
+- 1st Release
--- lib/serverkit/rbenv/version.rb
+++ lib/serverkit/rbenv/version.rb
@@ -1,5 +1,5 @@
 module Serverkit
   module Rbenv
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
