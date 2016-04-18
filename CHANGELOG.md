114cc9a20df6aa9c8965f919293b17c4a2204e9f
r7kamura <r7kamura@gmail.com>
2016-04-18 13:50:17 +0900

    v0.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.3.2
+- Fix Git dependencies for pacman
+
 ## 0.3.1
 - Fix wrong dependencies
 
--- lib/serverkit/rbenv/version.rb
+++ lib/serverkit/rbenv/version.rb
@@ -1,5 +1,5 @@
 module Serverkit
   module Rbenv
-    VERSION = "0.3.1"
+    VERSION = "0.3.2"
   end
 end
